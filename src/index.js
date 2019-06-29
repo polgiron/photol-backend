72b385390797dc56ec2108407d91c623da90864e
polgiron <pol.giron@gmail.com>
2019-06-29 16:46:44 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+// import models from './models';
+import models, { connectDb } from './models';
+import routes from './routes';
+
+const app = express();
+
+// import bodyParser from 'body-parser';
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// Body parser express built in
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cors ONLY DEV, needs whitelist
+app.use(cors());
+
+
+// ---
+// Middleware example
+
+app.use(async (req, res, next) => {
+  req.context = {
+    models,
+    me: await models.User.findByLogin('rwieruch'),
+  };
+  next();
+});
+
+
+// ---
+// Routes
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+
+// ---
+// Start server
+
+const eraseDatabaseOnSync = true;
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({}),
+    ]);
+
+    createUsersWithMessages();
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
+});
+
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'rwieruch',
+  });
+
+  const user2 = new models.User({
+    username: 'ddavids',
+  });
+
+  const message1 = new models.Message({
+    text: 'Published the Road to learn React',
+    user: user1.id,
+  });
+
+  const message2 = new models.Message({
+    text: 'Happy to release ...',
+    user: user2.id,
+  });
+
+  const message3 = new models.Message({
+    text: 'Published a complete ...',
+    user: user2.id,
+  });
+
+  await message1.save();
+  await message2.save();
+  await message3.save();
+
+  await user1.save();
+  await user2.save();
+};
