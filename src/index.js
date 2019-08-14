eb2ebc5ef702da320b9d655157393c487d8c197a
polgiron <pol.giron@gmail.com>
2019-08-15 00:13:56 +0100

    Enable cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Cors ONLY DEV, needs whitelist
-app.use(cors());
+app.use(cors({
+  origin: 'https://photol.paulgiron.com'
+}));
 
 
 // ---
