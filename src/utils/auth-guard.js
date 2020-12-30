e8164d2422f10a32287c1356c6089e0313a24436
Paul Giron <pol.giron@gmail.com>
2020-12-30 15:09:20 +0100

    Update packages

--- src/utils/auth-guard.js
+++ src/utils/auth-guard.js
@@ -2,5 +2,6 @@ import jwt from 'express-jwt';
 
 export const authGuard = jwt({
   secret: process.env.TOKEN_SECRET,
-  userProperty: 'payload'
+  userProperty: 'payload',
+  algorithms: ['sha1', 'RS256', 'HS256']
 });
